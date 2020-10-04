ca0b4d7a8885390e770f783c0143df737d9a4ec8
Nardacyon <63487094+Nardacyon@users.noreply.github.com>
2020-10-03 21:18:35 -0700

    created schema and seed

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("The Heart Attack");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Big Bang Burger", true);
+INSERT INTO burgers (burger_name) VALUES ("Meltburger");
