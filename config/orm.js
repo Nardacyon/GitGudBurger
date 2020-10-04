fb0dbb37e6b6c45e9006daf58ff283d6eded89d2
Nardacyon <63487094+Nardacyon@users.noreply.github.com>
2020-10-04 13:00:28 -0700

    refactored for easier use and readability

--- config/orm.js
+++ config/orm.js
@@ -1,36 +1,39 @@
 const connection = require("./connection");
 
+function helper(nums) {
+    const arr = [];
+
+    for (let i = 0; i < nums; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
 const orm = {
-    retrieve: function() {
-        return new Promise(function(resolve, reject) {
-            connection.query("SELECT * FROM burgers", (err, data) => {
-                if (err) reject (err);
-                resolve(data);
-            });
+    retrieve: function(table, cb) {
+        connection.query(`SELECT * FROM ??`, [table], (err, data) => {
+            if (err) throw err;
+            else cb(data);
         });
     },
-    create: function(burger_name) {
-        return new Promise(function(resolve, reject) {
-            connection.query("INSERT INTO burgers SET ?", burger_name, (err, data) => {
-                if (err) reject (err);
-                resolve(data);
-            });
+    create: function(table, columns, values, cb) {
+        connection.query(`INSERT INTO ?? (${columns.join()}) VALUES (${helper(values.length)})`, [table, values], (err, data) => {
+            if (err) throw (err);
+            else cb (data);
+        });
+
+    },
+    update: function(table, updateVal, id, cb) {
+        connection.query(`UPDATE ?? SET ${updateVal} WHERE id = ?`, [table, id], (err, data) => {
+            if (err) throw (err);
+            else cb(data);
         });
     },
-    update: function(id, devoured) {
-        return new Promise(function(resolve, reject) {
-            connection.query("UPDATE burgers SET devoured = ? WHERE id = ?", [devoured, id], (err, data) => {
-                if (err) reject (err);
-                resolve(data);
-            });
-        });
-    },
-    destroy: function(id) {
-        return new Promise(function(resolve, reject) {
-            connection.query("DELETE FROM burgers WHERE id = ?", id, (err, data) => {
-                if (err) reject (err);
-                resolve(data);
-            });
+    destroy: function(table, cb, id) {
+        connection.query(`DELETE FROM ?? WHERE id = ?`, [table, id], (err, data) => {
+            if (err) throw (err);
+            else cb(data);
         });
     }
 }
