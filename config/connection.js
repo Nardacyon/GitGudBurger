9ad063de4a79631e17f032a7dc4d1802a8abba2c
Nardacyon <63487094+Nardacyon@users.noreply.github.com>
2020-10-29 14:02:04 -0700

    added jawsdb connection. will be fully implemented in next update

--- config/connection.js
+++ config/connection.js
@@ -10,9 +10,24 @@ const connectionConfig = {
 
 const connection = mysql.createConnection(connectionConfig);
 
+// var connection;
+// if(process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL)
+// } else {
+//     connection = mysql.createConnection({
+//         host: "localhost",
+//         port: 3307,
+//         user: "root",
+//         password: "root",
+//         database: "burger_db"
+//     });
+// }
+
 connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected");
 })
+
+
 
 module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,6 @@
 const connection = require("./connection.js");
 
-function helper(nums) {
+function queryHelper(nums) {
     const arr = [];
 
     for (let i = 0; i < nums; i++) {
@@ -11,31 +11,68 @@ function helper(nums) {
 }
 
 const orm = {
-    retrieve: function(table, cb) {
-        connection.query("SELECT * FROM ??", [table], function(err, data) {
+    selectAll: function(cb) { 
+        connection.query("SELECT * FROM burgers", (err, data) => {
+            if(err) throw err;
+            cb(data);
+        });
+    },
+
+    insertOne: function(values, cb) {
+        connection.query(`INSERT INTO burgers (burger_name) VALUES ?`, values, (err, data) => {
             if (err) throw err;
             cb(data);
         });
     },
-    create: function(table, columns, values, cb) {
-        connection.query(`INSERT INTO ?? (${columns.join()}) VALUES (${helper(values.length)})`, [table, values], (err, data) => {
-            if (err) throw (err);
-            cb (data);
-        });
 
-    },
-    update: function(table, updateVal, id, cb) {
-        connection.query(`UPDATE ?? SET ${updateVal} WHERE id = ?`, [table, id], (err, data) => {
-            if (err) throw (err);
+    updateOne: function(id, cb) {
+        connection.query(`UPDATE burgers SET devoured = true WHERE id = ?`, id, (err, data) => {
+            if (err) throw err;
             cb(data);
         });
     },
-    destroy: function(table, cb, id) {
-        connection.query(`DELETE FROM ?? WHERE id = ?`, [table, id], (err, data) => {
-            if (err) throw (err);
+
+    resetAll: function(cb) {
+        connection.query(`UPDATE burgers SET devoured = false WHERE id > 0`, (err, data) => {
+            if(err) throw err;
+            cb(data);
+        });
+    },
+
+    deleteOne: function(id, cb) {
+        connection.query(`DELETE FROM burgers WHERE id = ${id}`, (err, data) => {
+            if(err) throw err;
             cb(data);
         });
     }
+    // retrieve: function(table, cb) {
+    //     connection.query("SELECT * FROM ??", [table], function(err, data) {
+    //         if (err) throw err;
+    //         cb(data);
+    //     });
+    // },
+
+    // create: function(table, columns, values, cb) {
+    //     connection.query(`INSERT INTO ?? (${columns.toString()}) VALUES (${queryHelper(values.length)})`, [table, values], (err, data) => {
+    //         if (err) throw (err);
+    //         cb(data);
+    //     });
+
+    // },
+
+    // update: function(table, updateVal, id, cb) {
+    //     connection.query(`UPDATE ?? SET ${updateVal} WHERE id = ?`, [table, id], (err, data) => {
+    //         if (err) throw (err);
+    //         cb(data);
+    //     });
+    // },
+
+    // destroy: function(table, cb, id) {
+    //     connection.query(`DELETE FROM ?? WHERE id = ?`, [table, id], (err, data) => {
+    //         if (err) throw (err);
+    //         cb(data);
+    //     });
+    // }
 }
 
 module.exports = orm;
